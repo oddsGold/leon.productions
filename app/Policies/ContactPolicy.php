da910dc19c8d89fdb312c82797d55d34c94d987d
verbraund <verbraund@gmail.com>
2024-09-09 11:46:14 +0300

    Contact popup admin backend fix

--- app/Policies/ContactPolicy.php
+++ app/Policies/ContactPolicy.php
@@ -2,7 +2,81 @@
 
 namespace App\Policies;
 
-class ContactPolicy extends BasePolicy
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ContactPolicy
 {
+    use HandlesAuthorization;
+
     protected $model = 'App\Models\Contact';
+
+    public function viewAny(User $user)
+    {
+        return $this->hasPermission($user, 'view') ||
+            $this->isAdmin($user) ||
+            $this->isSuperAdmin($user);
+    }
+
+    public function view(User $user)
+    {
+        return $this->hasPermission($user, 'view') ||
+            $this->isAdmin($user) ||
+            $this->isSuperAdmin($user);
+    }
+
+    public function create(User $user)
+    {
+        return $this->hasPermission($user, 'create') ||
+            $this->isAdmin($user) ||
+            $this->isSuperAdmin($user);
+    }
+
+    public function update(User $user)
+    {
+        return $this->hasPermission($user, 'update') ||
+            $this->isAdmin($user) ||
+            $this->isSuperAdmin($user);
+    }
+
+    public function delete(User $user)
+    {
+        return $this->hasPermission($user, 'delete') ||
+            $this->isAdmin($user) ||
+            $this->isSuperAdmin($user);
+    }
+
+    public function restore(User $user)
+    {
+        return $this->hasPermission($user, 'delete') ||
+            $this->isAdmin($user) ||
+            $this->isSuperAdmin($user);
+    }
+
+    public function forceDelete(User $user)
+    {
+        return $this->hasPermission($user, 'delete') ||
+            $this->isAdmin($user) ||
+            $this->isSuperAdmin($user);
+    }
+
+    protected function hasPermission($user, $name)
+    {
+        return $user->role->resource($this->getResource())->hasPermission($name);
+    }
+
+    protected function getResource()
+    {
+        return $this->model;
+    }
+
+    protected function isAdmin($user)
+    {
+        return $user->role->isAdmin();
+    }
+
+    protected function isSuperAdmin($user)
+    {
+        return $user->role->isSuperAdmin();
+    }
 }
