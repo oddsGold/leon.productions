aea1c551881f8b0d785870feba93db6a616a3556
verbraund <verbraund@gmail.com>
2024-09-12 00:10:46 +0300

    Fix social response admin

--- app/Services/About/AboutService.php
+++ app/Services/About/AboutService.php
@@ -85,15 +85,15 @@ class AboutService
     public function getSocialMedia()
     {
         return [
-            'instagram_link' => $this->dictionaryService->get('about_social_instagram_link', null),
+            'instagram_link' => (string)$this->dictionaryService->get('about_social_instagram_link', null),
             'instagram_published' => $this->dictionaryService->get('about_social_instagram_published', null),
-            'facebook_link' => $this->dictionaryService->get('about_social_facebook_link', null),
+            'facebook_link' => (string)$this->dictionaryService->get('about_social_facebook_link', null),
             'facebook_published' => $this->dictionaryService->get('about_social_facebook_published', null),
-            'youtube_link' => $this->dictionaryService->get('about_social_youtube_link', null),
+            'youtube_link' => (string)$this->dictionaryService->get('about_social_youtube_link', null),
             'youtube_published' => $this->dictionaryService->get('about_social_youtube_published', null),
-            'linkedin_link' => $this->dictionaryService->get('about_social_linkedin_link', null),
+            'linkedin_link' => (string)$this->dictionaryService->get('about_social_linkedin_link', null),
             'linkedin_published' => $this->dictionaryService->get('about_social_linkedin_published', null),
-            'vimeo_link' => $this->dictionaryService->get('about_social_vimeo_link', null),
+            'vimeo_link' => (string)$this->dictionaryService->get('about_social_vimeo_link', null),
             'vimeo_published' => $this->dictionaryService->get('about_social_vimeo_published', null),
         ];
     }
@@ -101,15 +101,15 @@ class AboutService
     public function updateSocialMedia($data)
     {
         return [
-            'instagram_link' => $this->dictionaryService->set('about_social_instagram_link', $data['instagram_link']),
+            'instagram_link' => (string)$this->dictionaryService->set('about_social_instagram_link', $data['instagram_link']),
             'instagram_published' => $this->dictionaryService->set('about_social_instagram_published', $data['instagram_published']),
-            'facebook_link' => $this->dictionaryService->set('about_social_facebook_link', $data['facebook_link']),
+            'facebook_link' => (string)$this->dictionaryService->set('about_social_facebook_link', $data['facebook_link']),
             'facebook_published' => $this->dictionaryService->set('about_social_facebook_published', $data['facebook_published']),
-            'youtube_link' => $this->dictionaryService->set('about_social_youtube_link', $data['youtube_link']),
+            'youtube_link' => (string)$this->dictionaryService->set('about_social_youtube_link', $data['youtube_link']),
             'youtube_published' => $this->dictionaryService->set('about_social_youtube_published', $data['youtube_published']),
-            'linkedin_link' => $this->dictionaryService->set('about_social_linkedin_link', $data['linkedin_link']),
+            'linkedin_link' => (string)$this->dictionaryService->set('about_social_linkedin_link', $data['linkedin_link']),
             'linkedin_published' => $this->dictionaryService->set('about_social_linkedin_published', $data['linkedin_published']),
-            'vimeo_link' => $this->dictionaryService->set('about_social_vimeo_link', $data['vimeo_link']),
+            'vimeo_link' => (string)$this->dictionaryService->set('about_social_vimeo_link', $data['vimeo_link']),
             'vimeo_published' => $this->dictionaryService->set('about_social_vimeo_published', $data['vimeo_published']),
         ];
     }
