2e4420ee977893af41d94a61b30ae01e1375c9cc
Sulym Dmytro <ds.intelserv@gmail.com>
2024-09-12 13:55:00 +0300

    Change loader

--- resources/assets/site/js/components/Loader/Loader.jsx
+++ resources/assets/site/js/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
 export default function Loader() {
     return (
-        <div className="loader-bg">
+        <div className="loader-bg-content">
             <div className="loader"></div>
         </div>
     )
