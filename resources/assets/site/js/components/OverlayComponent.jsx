50cd6a01f6d22b6cccbea308f1b2462fc98f68de
Sulym Dmytro <ds.intelserv@gmail.com>
2024-09-17 12:32:59 +0300

    handleOrientationChange cases

--- resources/assets/site/js/components/OverlayComponent.jsx
+++ resources/assets/site/js/components/OverlayComponent.jsx
@@ -1,30 +1,34 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { useVimeoPlayersFull } from "../hooks/useVimeoPlayersFull";
+import {useVimeoPlayersFull} from "../hooks/useVimeoPlayersFull";
 import VideoControls from "./Cases/VideoControls";
 import {useVimeoPlayersFullMobileFs} from "../hooks/useVimeoPlayersFullMobileFs";
 
-export default function OverlayComponent({ handleHideOverlay, selectedData }) {
+export default function OverlayComponent({handleHideOverlay, selectedData}) {
     const videoRefs = useVimeoPlayersFull([selectedData]);
     const videoMobileFsRefs = useVimeoPlayersFullMobileFs([selectedData]);
     const vimeoPlayerRef = useRef(null);
 
     useEffect(() => {
-        const handleOrientationChange = (orientation) => {
+        const handleOrientationChange = () => {
+            const orientation = window.screen.orientation.angle;
             if (vimeoPlayerRef.current) {
-                if (orientation !== 0) {
+                if (orientation === 90) {
+                    vimeoPlayerRef.current.classList.add('orientation-270');
+                } else if (orientation === -90 || orientation === 270) {
                     vimeoPlayerRef.current.classList.add('orientation');
                 } else {
                     vimeoPlayerRef.current.classList.remove('orientation');
+                    vimeoPlayerRef.current.classList.remove('orientation-270');
                 }
             }
         };
 
-        handleOrientationChange(window.orientation);
+        handleOrientationChange();
 
-        window.addEventListener('orientationchange', () => handleOrientationChange(window.orientation));
+        window.screen.orientation.addEventListener('change', handleOrientationChange);
 
         return () => {
-            window.removeEventListener('orientationchange', () => handleOrientationChange(window.orientation));
+            window.screen.orientation.removeEventListener('change', handleOrientationChange);
         };
     }, []);
 
