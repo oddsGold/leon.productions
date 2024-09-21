36c5e25ff6f331f408570684185fcc886c64d39f
verbraund <verbraund@gmail.com>
2024-09-21 17:09:40 +0300

    Change create video case request

--- app/Http/Requests/Admin/VideoCase/CaseUpdateRequest.php
+++ app/Http/Requests/Admin/VideoCase/CaseUpdateRequest.php
@@ -2,7 +2,25 @@
 
 namespace App\Http\Requests\Admin\VideoCase;
 
-class CaseUpdateRequest extends CaseCreateRequest
-{
+use App\Http\Requests\Admin\BaseRequest;
 
+class CaseUpdateRequest extends BaseRequest
+{
+    public function rules()
+    {
+        return [
+            'slug' => 'required|string|min:3|max:255|not_in:showreel',
+            'description' => 'required|string|max:255',
+            'preview_url' => 'nullable|string|max:150',
+            'main_url' => 'required|string|max:150',
+            'image' => 'nullable',
+            'image.id' => 'nullable|integer',
+            'image.name' => 'nullable|string',
+            'image.path' => 'nullable|string',
+            'image.url' => 'nullable|string',
+            'published' => 'required|boolean',
+            'published_at' => 'nullable|date_format:Y-m-d H:i:s',
+            'published_to' => 'nullable|date_format:Y-m-d H:i:s'
+        ];
+    }
 }
